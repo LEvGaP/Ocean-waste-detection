75ab4e23785f81d151deff3344396c2767111913
Alexey_Dzhevello <fishwere@mail.ru>
2024-12-27 04:58:09 +0500

    removed imports

--- utils/maps.py
+++ utils/maps.py
@@ -1,8 +1,27 @@
-# from threaded_loader import ThreadedLoader
-from console_helpers import run_console_command_with_life_output
 
+import subprocess
+
+def run_console_command_with_life_output(command: list):
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    try:
+        while True:
+            output = process.stdout.readline()
+            if output == '' and process.poll() is not None:
+                break
+            if output:
+                print(output.strip())
+    finally:
+        process.stdout.close()
+        process.wait()
+    pass
 
 def load_total_label_map():
     command = ["curl", "https://drive.usercontent.google.com/download?id=1hGpT4UYSNtfEXayuzcRJzE5XkFVRVd3m&confirm=xxx", "-o", "total_label_map.json"]
     run_console_command_with_life_output(command)
+    pass
+
+# from threaded_loader import ThreadedLoader
+
+def load_photos():
+    ThreadedLoader.get_instance().download_folder_threaded('photos')
     pass
